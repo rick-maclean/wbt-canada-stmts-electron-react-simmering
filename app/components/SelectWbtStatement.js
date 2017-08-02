efc4c61fcf7bec634214e9016cee184e2db6d136
Rick MacLean <rick_maclean@sil.org>
2017-08-02 16:59:28 -0500

    make changes for selection of WBT statement file html

--- app/components/SelectWbtStatement.js
+++ app/components/SelectWbtStatement.js
@@ -11,56 +11,27 @@ const dialog = app.dialog;
 
 class SelectWbtStatement extends Component {
   state: {
-    metaDataFolder: string,
-    metaDataFolderSelected: boolean,
-    jobFilepath: string,
-    jobFilepathSelected: boolean
+    wbtStatementFile: string,
   }
   constructor() {
     super();
     this.state = {
-      metaDataFolder: 'heyhey',
-      metaDataFolderSelected: false,
-      jobFilepath: 'jobFilepath',
-      jobFilepathSelected: false
+      wbtStatementFile: 'wbtStatementFile',
     };
   }
-  selectJobSpecsFile = () => {
-    console.log('called selectJobSpecsFile');
+  selectWbtStatementFile = () => {
+    console.log('called selectWbtStatementFile');
     const fileNames = dialog.showOpenDialog();
     if (fileNames === undefined) {
       console.log('No file selected');
-      this.setState({ jobFilepathSelected: false });
-      this.setState({ jobFilepath: '' });
+      this.setState({ wbtStatementFile: '' });
     } else {
       // console.log('going to set the filename and boolean' + fileNames);
-      this.setState({ jobFilepathSelected: true });
-      this.setState({ jobFilepath: fileNames[0] });
+      this.setState({ wbtStatementFile: fileNames[0] });
     }
     // NOTE put this back in when the first part is working
     // persistData('jobFilePersistKey', fileNames[0]);
-    console.log('end of selectJobSpecsFile');
-  }
-  selectMetaDataFile = () => {
-    console.log('called selectMetaDataFile');
-    const path = dialog.showOpenDialog(
-      { title: 'Select a folder', properties: ['openDirectory'] }
-    );
-    console.log('called showOpenDialog and returned');
-    console.log('path is =====>' + path);
-    if (path === undefined) {
-      console.log('No destination folder selected');
-      this.setState({ metaDataFolderSelected: false });
-      this.setState({ metaDataFolder: '' });
-    } else {
-      console.log('path returned from dialog.showOpenDialog select a folder is....> ' + path);
-      // console.log('path[0] is....> ' + path[0]);
-      this.setState({ metaDataFolderSelected: true });
-      this.setState({ metaDataFolder: path[0] });
-    }
-    // NOTE put this back in when the first part is working
-    // persistData('metaDataPersistKey', path[0]);
-    console.log('end of selectMetaDataFile');
+    console.log('end of selectWbtStatementFile');
   }
   render() {
     /*
@@ -70,47 +41,22 @@ class SelectWbtStatement extends Component {
       $('#sendButton').attr('disabled', 'true');
     }
     */
-    let itemButton;
-    if (this.state.jobFilepathSelected && this.state.metaDataFolderSelected) {
-      itemButton = <button type="submit" id="sendButton" className="btn btn-primary pull-left">Send</button>
-    } else {
-      itemButton = <button disabled="true" type="submit" id="sendButton" className="btn btn-primary pull-left">Send</button>
-    }
     return (
       <div className="panel panel-primary">
-        <div className="panel-heading apt-addheading">Job</div>
+        <div className="panel-heading apt-addheading">Select WBT Statement</div>
         <div className="panel-body">
           <form className="form" onSubmit={this.localHandleSend}>
-            <h3>Job</h3>
             <div className="form-group">
-              <label htmlFor="metaDataFolder">Metadata File</label>
-              <div className="form-text" id="metaDataFolderId" >{this.state.metaDataFolder}</div>
+              <label htmlFor="wbtStatementFile">WBT Statement</label>
+              <div className="form-text" id="wbtStatementFileId" >{this.state.wbtStatementFile}</div>
               <div className="col-sm-offset-3 col-sm-9">
                 <div className="pull-right">
                   <button
                     type="button"
                     className="btn btn-primary"
-                    onClick={this.selectMetaDataFile}
-                  >Select metadata.xml</button>&nbsp;
+                    onClick={this.selectWbtStatementFile}
+                  >Select WBT Statement html file</button>&nbsp;
                 </div>
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="metaDataFolder">Job Spec</label>
-              <div className="form-text" id="metaDataFolderId" >{this.state.jobFilepath}</div>
-              <div className="col-sm-offset-3 col-sm-9">
-                <div className="pull-right">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={this.selectJobSpecsFile}
-                  >Select job specification</button>&nbsp;
-                </div>
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
-                {itemButton}
               </div>
             </div>
           </form>
